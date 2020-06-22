f275d14a6f3a1da9c6b95fc8a951095ef38ba4bb
CalebSLane <c.steelelane@gmail.com>
2020-06-22 13:25:10 -0700

    read ssl config from hapi.properties and add Porduction dockerfile

--- src/main/java/ca/uhn/fhir/jpa/starter/HttpClientConfig.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/HttpClientConfig.java
@@ -21,7 +21,7 @@ import ca.uhn.fhir.rest.client.api.IRestfulClientFactory;
 import ca.uhn.fhir.to.TesterConfig;
 
 @Configuration
-@PropertySource("file:/run/secrets/common.properties")
+@PropertySource("file:/run/secrets/hapi.properties")
 public class HttpClientConfig {
 
 	@Value("${server.ssl.trust-store}")
